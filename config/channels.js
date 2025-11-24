5fd99df59278ebd8bae29d0f1432b8a84f5ce9fe
Shikhar Yadav <92710243+shikharyadav16@users.noreply.github.com>
2025-11-24 07:58:38 +0530

    Update channels.js

--- config/channels.js
+++ config/channels.js
@@ -2,6 +2,6 @@ const resultChannelId = "1437283979245125704";
 const playersChannelId = "1437720976086401064";
 const reportChannelId = "1206669235229827072";
 const author = "1094004706822590464";
-const newResId = "1441007507974914153";
+const newResId = "1442340723071385690";
 
 module.exports = { resultChannelId, playersChannelId, newResId, reportChannelId, author };
